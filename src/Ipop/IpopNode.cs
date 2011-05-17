a7fb70b6df806d092ef507c844d0cd8fbb9626ca
David Wolinsky <davidiw@ufl.edu>
2011-05-17 12:49:13 -0400

    [Ipop] Call Set on OnDemand for both outgoing and incoming messages

--- src/Ipop/IpopNode.cs
+++ src/Ipop/IpopNode.cs
@@ -257,6 +257,10 @@ namespace Ipop {
 
       if(ret is AHSender) {
         addr = ((AHSender) ret).Destination;
+        if(addr == null) {
+          ProtocolLog.Write(IpopLog.PacketLog, "Null Address from AHSender");
+          return;
+        }
       } else {
         ProtocolLog.Write(IpopLog.PacketLog, String.Format(
           "Incoming packet was not from an AHSender: {0}.", ret));
@@ -301,6 +305,7 @@ namespace Ipop {
       }
 
       WriteIP(packet);
+      _ondemand.Set(addr);
     }
 
     /// <summary>This method handles IPPackets that come from the TAP Device, i.e.,
